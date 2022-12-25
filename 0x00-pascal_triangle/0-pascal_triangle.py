63e754ba1af373679a7c5fd4cbd282ec09de8b3c
Moses Oyet David <trickmaxillar@gmail.com>
2022-12-25 17:20:09 +0300

    Remove function call

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,13 +6,11 @@ A program to print pascal triangle till n'th number
 def pascal_triangle(n):
     """
     print n'th number of pascal triangle
-    >>> if n <= 0:
-    >>>     return 0;
+    if n <= 0:
+    return 0;
     """
     if n <= 0:
         return []
     for i in range(n):
         print(' '*(n-1), end="")
         print(' '.join(map(str, str(11**i))))
-
-pascal_triangle(5)
