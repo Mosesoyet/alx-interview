2db7b41a2bb6a5f36b09222c8b109ca4749c6b0e
Moses Oyet David <trickmaxillar@gmail.com>
2022-12-25 18:24:23 +0300

    Modify the whole function,

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,8 +9,14 @@ def pascal_triangle(n):
     if n <= 0:
     return 0;
     """
-    if n <= 0:
-        return []
+    res = []
     for i in range(n):
-        print(' '*(n-1), end="")
-        print(' '.join(map(str, str(11**i))))
+        if (n <= 0) : return []
+        temp_list = []
+        for j in range(i):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(res[i-1][j-1] + res[i-1][j])
+        res.append(temp_list)
+    return res
