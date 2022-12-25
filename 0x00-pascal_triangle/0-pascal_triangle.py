eb10d4d105886499ade646a1d9cd9e017b008973
Moses Oyet David <trickmaxillar@gmail.com>
2022-12-25 18:35:07 +0300

    Executablility added to file and some comments

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,18 @@
 A program to print pascal triangle till n'th number
 """
 
+
 def pascal_triangle(n):
     """
     print n'th number of pascal triangle
-    if n <= 0:
-    return 0;
+    if n <= 0
+    return 0
     """
+    # Declare an empty list to store results
     res = []
     for i in range(n):
         if (n <= 0) : return []
+        # a temporary list used for iteration
         temp_list = []
         for j in range(i+1):
             if j == 0 or j == i:
