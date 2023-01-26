7d6c56f4c53577271fa0b9796a5533f1c9de7772
= <=>
2023-01-26 18:26:10 +0000

    Modify

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,18 +8,19 @@ def validUTF8(data):
     """A method that determines if a given data set represents a valid UTF-8 encoding
     """
     num_bytes = 0
-    for n in data:
-        mask = 1 << 7
-        if not num_bytes:
-            while mask & n:
-                num_bytes += 1
-                mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
+
+    for i in data:
+        if num_bytes == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                num_bytes = 1
+            elif i >> 4 == 0b1110:
+                num_bytes = 2
+            elif i >> 3 == 0b11110:
+                num_bytes = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            if n >> 6 != 0b10:
+            if i >> 6 != 0b10:
                 return False
             num_bytes -= 1
     return num_bytes == 0
