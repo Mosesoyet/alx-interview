747dfd4211b9226dd5d94a46f7a821568d1d2374
= <=>
2023-02-15 04:23:50 -0800

    Add return statement

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,8 @@ Rotate 2d Matrix modules
 """
 
 
-def rotate_2d_matrix(matrix) -> None:
+def rotate_2d_matrix(matrix):
     """ Print 2d matrix and rotates it to 90 deg
     """
     matrix[:] = [[row[i] for row in matrix[::-1]] for i in range(len(matrix))]
+    return None
