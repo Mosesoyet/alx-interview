66021776c5ef87e85e58c8e317693e962902dacb
= <=>
2023-02-15 04:19:37 -0800

    Use list comprehension

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,4 +7,4 @@ Rotate 2d Matrix modules
 def rotate_2d_matrix(matrix) -> None:
     """ Print 2d matrix and rotates it to 90 deg
     """
-    matrix[:] = zip(*matrix[::-1])
+    matrix[:] = [[row[i] for row in matrix[::-1]] for i in range(len(matrix))]
