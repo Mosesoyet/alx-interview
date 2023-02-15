dd44be99eece248abbe53945a4020e8ffd843120
= <=>
2023-02-15 03:47:31 -0800

    Remove imports

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,13 +3,8 @@
 Rotate 2d Matrix modules
 """
 
-from typing import List
 
-
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix) -> None:
     """ Print 2d matrix and rotates it to 90 deg
     """
-    new_list = []
-    for i in zip(*matrix):
-        new_list.append(i[::-1])
-        matrix[:] = new_list
+    matrix[:] = zip(*matrix[::-1])
---
+++ 0x07-rotate_2d_matrix/soln_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 
+Rotate 2d Matrix modules
+"""
+
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """ Print 2d matrix and rotates it to 90 deg
+    """
+    new_list = []
+    for i in zip(*matrix):
+        new_list.append(i[::-1])
+        matrix[:] = new_list
