5fb3d785f1aa01a0e622ab6156442d494509b709
= <=>
2023-02-15 23:47:05 -0800

    Make file executable

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,7 +5,7 @@ Rotate 2d Matrix modules
 
 
 def rotate_2d_matrix(matrix):
-    """ Print 2d matrix and rotates it to 90 deg
+    """ Print 2d matrix and rotates it to 90 degree
     """
     matrix[:] = [[row[i] for row in matrix[::-1]] for i in range(len(matrix))]
     return None
